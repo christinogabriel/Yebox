689eb32ab6ce66b35c4ea592fce553130ad0458d
Gabriel Christino Dos Santos <JAC0057474@aluno.firjansenaisesi.com.br>
2023-08-31 12:08:12 -0300

    Criação de Projeto

--- js/main.js
+++ js/main.js
@@ -10,8 +10,19 @@ let Start = setInterval(()=>{
     // Add your methods for Sanddox here
     YeEngine.AlterThemeSystem(YeEngine.Theme.DarkTheme)
     YeEngine.AlterTheme(YeEngine.Theme.Temacinza)
+    YeEngine.ModifyW(document.getElementById("card"),"80vh")
+    YeEngine.ModifyH(document.getElementById("card"),"80vh")
 
 
     //End Your methods Here
     clearInterval(Start) //Dont erase this line
 },400)
+
+let Update = setInterval(()=>{
+    if (YeEngine.Questions_save[0]!= undefined){
+        YeEngine.ModifyContentClass(document.getElementById("card"),YeEngine.Questions_save[0])
+        
+        clearInterval(Update)
+    }
+    
+},600)
